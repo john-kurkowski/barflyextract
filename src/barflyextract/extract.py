4accf95454b6159d7a5d78f72c2f26c91a2c2042
John Kurkowski <john.kurkowski@gmail.com>
2019-09-08 14:47:35 -0700

    Fix multiline regex
    Fixes many missed recipe paragraphs.

--- src/barflyextract/extract.py
+++ src/barflyextract/extract.py
@@ -7,7 +7,7 @@ import unidecode
 
 from barflyextract.util import partition
 
-MEASURE_RE = re.compile(r"^\S*\d(oz|ml)")
+MEASURE_RE = re.compile(r"^\S*\d\s*(oz|ml)", re.MULTILINE)
 PARAGRAPHS_RE = re.compile(r"\n{2,}")
 TYPE_NAME_RE = re.compile(r"(?P<type>.*):\s*(?P<name>.*)")
 URL_RE = re.compile(r"\bhttps?://")
