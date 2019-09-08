d2862926f1b115ea9045434c2a0426aed3890535
John Kurkowski <john.kurkowski@gmail.com>
2019-09-07 23:04:11 -0700

    Extract paragraphs with measurements

--- src/barflyextract/extract.py
+++ src/barflyextract/extract.py
@@ -1,16 +1,35 @@
 import json
+import re
 import sys
+
+MEASURE_RE = re.compile(r"\d(oz|ml)")
+PARAGRAPHS_RE = re.compile(r"\n{2,}")
 
 
 def print_markdown(items):
     for item in items:
         print(f"# {item['title']}")
         print()
-        print(f"# {item['description']}")
+        print(f"# {item['recipe']}")
+        print()
+
+
+def process(item):
+    paras = PARAGRAPHS_RE.split(item["description"])
+    recipe_start_i = next(
+        (i for i, s in enumerate(paras) if MEASURE_RE.search(s)), None
+    )
+    if recipe_start_i is None:
+        return None
+
+    recipe = "\n\n".join(paras[recipe_start_i:])
+
+    item["recipe"] = recipe
+    return item
 
 
 def run():
-    items = json.load(sys.stdin)
+    items = filter(bool, [process(item) for item in json.load(sys.stdin)])
     print_markdown(items)
 
 
