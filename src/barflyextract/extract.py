034b3a90b164e0fc30cf7a5763c04594e01df202
John Kurkowski <john.kurkowski@gmail.com>
2019-09-08 12:52:58 -0700

    Add logging

--- src/barflyextract/extract.py
+++ src/barflyextract/extract.py
@@ -1,4 +1,5 @@
 import json
+import logging
 import re
 import sys
 
@@ -20,8 +21,15 @@ def process(item):
         (i for i, s in enumerate(paras) if MEASURE_RE.search(s)), None
     )
     if recipe_start_i is None:
+        logging.debug("""No recipe found in "%s". Skipping.""", item["title"])
         return None
 
+    logging.debug(
+        """Recipe found in "%s" at paragraph %d. Taking it and remaining %d paragraphs.""",
+        item["title"],
+        recipe_start_i,
+        len(paras) - recipe_start_i - 1,
+    )
     recipe = "\n\n".join(paras[recipe_start_i:])
 
     item["recipe"] = recipe
@@ -29,7 +37,9 @@ def process(item):
 
 
 def run():
-    items = filter(bool, [process(item) for item in json.load(sys.stdin)])
+    logging.basicConfig(level=logging.DEBUG)
+
+    items = filter(bool, (process(item) for item in json.load(sys.stdin)))
     print_markdown(items)
 
 
