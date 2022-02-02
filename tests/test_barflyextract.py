c3cc5c4cad6b7b4f4da7e13c31eba7be4a484c42
John Kurkowski <john.kurkowski@gmail.com>
2022-02-01 18:44:37 -0800

    Restore tests folder
    To fix CI.

---
+++ tests/test_barflyextract.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+
+import barflyextract
+
+__author__ = "John Kurkowski"
+__copyright__ = "John Kurkowski"
+__license__ = "proprietary"
+
+
+def test_at_least_one_test_case():
+    assert barflyextract
